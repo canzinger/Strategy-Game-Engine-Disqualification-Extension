0416482bb6db1160c7f85c32d31aa10e5531c6c6
Lukas Grassauer <lukas@grassauer.eu>
2019-04-15 17:34:05 +0200

    add fully functional HumanAgent

--- src/main/java/dev/entze/sge/agent/HumanAgent.java
+++ src/main/java/dev/entze/sge/agent/HumanAgent.java
@@ -1,12 +1,71 @@
 package dev.entze.sge.agent;
 
 import dev.entze.sge.game.Game;
+import dev.entze.sge.util.Pair;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public final class HumanAgent<G extends Game<A, ?>, A> implements GameAgent<G, A> {
 
   @Override
   public A calculateNextAction(G game, long calculationTime, TimeUnit timeUnit) {
-    return null;
+
+    List<Pair<Integer, A>> previousActions = game.getPreviousActions();
+    int lastActionOfThisPlayer = (-1);
+    for (int i = previousActions.size() - 1; i >= 0 && lastActionOfThisPlayer < 0; i--) {
+      if (previousActions.get(i).getA() == game.getCurrentPlayer()) {
+        lastActionOfThisPlayer = i;
+      }
+    }
+
+    if (lastActionOfThisPlayer < previousActions.size() - 1) {
+      System.out.println("Last moves:");
+      for (int i = lastActionOfThisPlayer + 1; i < previousActions.size(); i++) {
+        System.out.print("(" + i + ") Player " + previousActions.get(i).getA() + ": ");
+        System.out.println(previousActions.get(i).getB().toString() + '\n');
+      }
+    }
+
+    System.out.println("Possible actions:");
+    List<A> possibleActions = new ArrayList<>(game.getPossibleActions());
+
+    for (int i = 0; i < possibleActions.size(); i++) {
+      System.out.println("[" + i + "] " + possibleActions.get(i).toString());
+    }
+
+    int move = (-1);
+    String input = null;
+
+    while (!(0 <= move && move < possibleActions.size())) {
+      System.out.print("\n0.." + (possibleActions.size() - 1) + " > ");
+      if (System.console() != null) {
+        input = System.console().readLine();
+      } else {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+          input = bufferedReader.readLine();
+        } catch (IOException e) {
+          System.err.println("Could not read input.");
+        }
+      }
+
+      if (input != null) {
+        try {
+          move = Integer.parseInt(input);
+        } catch (NumberFormatException ignored) {
+        }
+
+        if (!(0 <= move && move < possibleActions.size())) {
+          System.err.println("Not a valid number.");
+        }
+      }
+
+    }
+
+    return possibleActions.get(move);
   }
 }
